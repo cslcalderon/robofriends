e3ee2fbcde75e633dfe7d5beaf649a7bd705a356
sofia calderon <cslcalderon@gmail.com>
2023-11-09 09:50:23 -0600

    making Hello Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +17,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning React
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// // Create a root.
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// // Initial render: Render the <Hello /> component to our root.
+// root.render(<Hello />);
+
+//doing it on one line
+ReactDOM.createRoot(document.getElementById('root')).render(<Hello />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
