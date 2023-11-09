115a302d12f7647b3910bac461bf8e1a4b18015d
sofia calderon <cslcalderon@gmail.com>
2023-11-09 10:47:31 -0600

    making card and cardlist components

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Hello.css';
 
 class Hello extends Component {
     render () {
         return (
-        <div>
+        <div className = 'f1 tc'>
             <h1> Hello World </h1>
-            <p> Welcome to React</p>
+            <p> {this.props.greeting}</p>
         </div>     
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Hello from './Hello';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots';
 
 // // Create a root.
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,9 +13,9 @@ import reportWebVitals from './reportWebVitals';
 // root.render(<Hello />);
 
 //doing it on one line
-ReactDOM.createRoot(document.getElementById('root')).render(<Hello />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <CardList robots={robots}/>
+   
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
