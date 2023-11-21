0a4ffa1276ddd8c5689b4fddfd47ebe11886524f
sofia calderon <cslcalderon@gmail.com>
2023-11-21 13:35:17 -0600

    cleaning up code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import {robots} from './robots';
 
 // // Create a root.
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,9 +12,6 @@ import {robots} from './robots';
 // root.render(<Hello />);
 
 //doing it on one line
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App robots={robots}/>
-   
-);
+ReactDOM.createRoot(document.getElementById('root')).render( <App/>);
 
 reportWebVitals();
